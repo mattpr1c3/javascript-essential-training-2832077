d3ed5c839d5a4b8cce5a8c737d0f5b1091a05c2d
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-03 07:01:32 -0700

    did exercises

--- 07_04/script.js
+++ 07_04/script.js
@@ -8,4 +8,23 @@
  * - Remove the item you found using the find method from the array.
  */
 
+const newArray = ["bottle", "cup", "pen", "paper", "mouse", "phone", "magnet", "candy"];
+
+// console.log(newArray.pop())
+
+console.log(newArray.unshift(newArray.pop()))
+console.log(newArray)
+
+console.log(newArray.sort())
+
+const found = newArray.find((item) => {
+    if (item.length >= 6)
+    return item
+});
+console.log(found)
+
+newArray.splice(newArray.indexOf("bottle"), 1)
+console.log(newArray)
+
+
 
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,12 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+document.querySelector("main").classList.add("new-class", "newer-class")
+
+document.querySelector("main").classList.remove("newer-class")
+
+document.querySelector("main").setAttribute("title", "this shouldn't be here")
+
+document.querySelector("main").getAttribute("title")
+
+document.querySelector("main").setAttribute("title", "we don't need a title here")
